cbaa539b3cb18e043fbc839ce8ff8b78b2bf433b
tansukureysi <tansu.kureysi@gmail.com>
2022-08-22 16:36:16 +0300

    c#kargotakip
    c# da sql bağlantısı yaparak sisteme giriş ve kargo takibini sağlama

---
+++ kargotakip/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace kargotakip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection baglan; //sql ile bağlantıyı sağlar.
+        SqlCommand komut; // C# ile sql kodlarını birleştirir.karşılaştırma
+        SqlDataReader oku; //sql verileri okuma işlemi yapar
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sorgu = "Select * from login where kullanıcıadı=@kullanıcıadı AND şifre=@şifre";
+            baglan = new SqlConnection("server=LAPTOP-B7T40IKF\\SQLEXPRESS01; Initial Catalog=kargo; Integrated Security=True");
+            komut = new SqlCommand(sorgu, baglan);
+            komut.Parameters.AddWithValue("@kullanıcıadı", textBox1.Text);
+            komut.Parameters.AddWithValue("@şifre", textBox2.Text);
+            baglan.Open();
+            oku = komut.ExecuteReader();//komut içindeki okumalar
+            if (oku.Read())
+            {
+                MessageBox.Show("Başarılı Giriş");
+                Form2 f2 = new Form2();
+                f2.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Kullanıcı adı veya şifreyi yanlış girdiniz");
+
+
+
+            }
+            baglan.Close();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                textBox2.PasswordChar = '\0';
+            }
+            else
+            {
+                textBox2.PasswordChar = '*';
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox2.PasswordChar = '*';
+        }
+    }
+}
---
+++ kargotakip/Form2.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace kargotakip
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglan = new SqlConnection("server=LAPTOP-B7T40IKF\\SQLEXPRESS01; Initial Catalog=kargo; Integrated Security =True");
+
+        private void veri()
+        {
+            baglan.Open();
+            string getir = "select * from kargotakip";
+            SqlDataAdapter da = new SqlDataAdapter(getir, baglan);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            baglan.Close();
+           
+        }
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            veri();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            baglan.Open();
+            SqlCommand ekle = new SqlCommand("insert into kargotakip (isim,soyisim,telefon,adres,durum) values(@isim,@soyisim,@telefon,@adres,@durum)", baglan);
+            ekle.Parameters.AddWithValue("@isim", textBox2.Text);
+            ekle.Parameters.AddWithValue("@soyisim", textBox3.Text);
+            ekle.Parameters.AddWithValue("@telefon", textBox4.Text);
+            ekle.Parameters.AddWithValue("@adres", textBox5.Text);
+            ekle.Parameters.AddWithValue("@durum", textBox6.Text);
+            ekle.ExecuteNonQuery();
+            baglan.Close();
+            veri();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            baglan.Open();
+            SqlCommand guncelle = new SqlCommand("update kargotakip set isim=@isim,soyisim=@soyisim,telefon=@telefon,adres=@adres,durum=@durum", baglan);
+            guncelle.Parameters.AddWithValue("@isim", textBox2.Text);
+            guncelle.Parameters.AddWithValue("@soyisim", textBox3.Text);
+            guncelle.Parameters.AddWithValue("@telefon", textBox4.Text);
+            guncelle.Parameters.AddWithValue("@adres", textBox5.Text);
+            guncelle.Parameters.AddWithValue("@durum", textBox6.Text);
+            guncelle.ExecuteNonQuery();
+            baglan.Close();
+            veri();
+
+            MessageBox.Show("güncelleme tamamlandı.");
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            baglan.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter("select * from kargotakip where durum='yolda'", baglan);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+            baglan.Close();
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            baglan.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter("select * from kargotakip", baglan);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+            baglan.Close();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            baglan.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter("select * from kargotakip where durum='teslim'", baglan);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+            baglan.Close();
+
+        }
+    }
+}
